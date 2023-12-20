e715133d71becddc3fd3b40bd523dcfadb0f4974
burillu <burillu90@hotmail.it>
2023-12-20 17:17:32 +0100

    improve stle and condition in movie control

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -16,7 +16,7 @@ class MovieController extends Controller
     }
     public function show($id)
     {
-        if ($id >= 0 && $id < count(Movie::all())) {
+        if ($id >= 0 && $id <= count(Movie::all())) {
             # code...
             $movie = Movie::findOrFail($id);
             return view("movies.show", compact("movie"));
