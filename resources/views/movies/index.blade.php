602fd8a6337755513b697063ab3e29913401c7da
burillu <burillu90@hotmail.it>
2023-12-20 17:44:17 +0100

    improve style in movie index and show

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -6,14 +6,14 @@
 
 
     <div class="container">
-        <div class="row">
+        <div class="row gy-4">
             @foreach ($movies as $item)
                 <div class="col-12 col-md-4 col-lg-3">
                     <div class="card">
                         <img src="{{ $item->image }}" class="card-img-top" alt="{{ $item->title }}">
                         <div class="card-body">
                             <h5 class="card-title">{{ $item->title }}</h5>
-                            <a href="{{ route('movies.show', $item->id) }}" class="btn btn-primary">Go somewhere</a>
+                            <a href="{{ route('movies.show', $item->id) }}" class="btn btn-primary">Movie Deails</a>
                         </div>
                     </div>
                 </div>
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -7,7 +7,7 @@
     <div class="container">
         <div class="row">
 
-            <div class="col-12 col-md-4 col-lg-3">
+            <div class="col-12 col-md-4">
                 <div class="card">
                     <img src="{{ $movie->image }}" class="card-img-top" alt="{{ $movie->title }}">
                     <div class="card-body">
@@ -15,6 +15,19 @@
                         <a href="{{ route('movies') }}" class="btn btn-primary">Back to Movie List</a>
                     </div>
                 </div>
+            </div>
+            <div class="col p-5">
+                <h2>{{ $movie->title }}</h2>
+                <h4 class="fstl-italic"> Original Name: {{ $movie->original_title }} </h4>
+                <span class="fw-bold"> Vote : {{ $movie->vote }} </span>
+                <p class="mt-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis repudiandae error consequuntur
+                    dolorem aperiam molestias, voluptas magnam quidem! Inventore autem aliquam amet obcaecati quaerat, a
+                    nemo deserunt? Quas qui ipsum eligendi quis quibusdam error perferendis ducimus, dolore animi dicta
+                    exercitationem corporis ea? Labore recusandae velit incidunt at, facilis impedit ea?
+                </p>
+
+
             </div>
 
         </div>
