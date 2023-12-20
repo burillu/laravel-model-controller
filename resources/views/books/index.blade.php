29b521f008790b58725bb90274f9c1cdca2c517a
burillu <burillu90@hotmail.it>
2023-12-20 18:28:02 +0100

    add content in index and show

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,15 +3,22 @@
 @section('title', 'Books')
 
 @section('content')
-<div class="container">
-    <h2>Suggest Books</h2>
-    <div class="row">
-        
-        
+    <div class="container">
+        <div class="row gy-4">
+            @foreach ($books as $item)
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img src="{{ $item->cover_image }}" class="card-img-top" alt="{{ $item->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $item->title }}</h5>
+                            <a href="{{ route('books.show', $item->id) }}" class="btn btn-primary">Book Deails</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 
-</div>
- 
 
 
 
--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -4,7 +4,31 @@
 
 @section('content')
 
- <h2>Variable content</h2>
+    <div class="container">
+        <div class="row gy-4">
+
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <img src="{{ $book->cover_image }}" class="card-img-top" alt="{{ $book->title }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $book->title }}</h5>
+                        <a href="{{ route('books') }}" class="btn btn-primary">Back to Books</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col p-5">
+                <h2>{{ $book->title }}</h2>
+
+
+                <p class="mt-5">
+                    {{ $book->plot }}
+                </p>
+
+
+            </div>
+
+        </div>
+    </div>
 
 
 @endsection
