5d60bb492b8e3e2c36c9d87ed3fc8ee4d77fc92f
burillu <burillu90@hotmail.it>
2023-12-20 18:28:52 +0100

    add content in home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
+use App\Models\Book;
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
-        return view("home");
+    public function index()
+    {
+        $movies = Movie::all();
+        $movie = $movies[rand(0, count($movies) - 1)];
+        $book = Book::all()[rand(0, count(Book::all()) - 1)];
+        return view("home", compact("movie", "book"));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,33 @@
 
 @section('content')
 
- <h2>Variable content</h2>
+    <div class="container">
+        <h2>Our Suggest for today</h2>
+        <di class="row justify-content-evenly">
+
+            <div class="col-4">
+                <h4 class="text-center">Movie</h4>
+                <div class="card">
+                    <img src="{{ $movie->image }}" class="card-img-top" alt="{{ $movie->title }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <a href="{{ route('movies') }}" class="btn btn-primary">All Movie</a>
+                    </div>
+                </div>
+            </div>
+            {{-- books --}}
+            <div class="col-4">
+                <h4 class="text-center">Book</h4>
+                <div class="card">
+                    <img src="{{ $book->cover_image }}" class="card-img-top" alt="{{ $movie->title }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $book->title }}</h5>
+                        <a href="{{ route('books') }}" class="btn btn-primary">All Books</a>
+                    </div>
+                </div>
+            </div>
+        </di>
+    </div>
 
 
 @endsection
