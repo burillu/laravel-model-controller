3aed9946ee36fda16ba1e1f82e548fb87facc5eb
burillu <burillu90@hotmail.it>
2023-12-20 15:45:06 +0100

    scaffolding, model and control movie, connect db ok

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/movies', [MovieController::class,'index'])->name('movies');
